ed9846991023f82fcd4f997ebd08524f99f0a027
BreElle Wells <breellejordyn@gmail.com>
2023-07-31 21:58:16 -0600

    complete links deliverables

--- src/components/About.js
+++ src/components/About.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Links from './Links'
 
 function About({ bio, links }) {
   return (
@@ -6,7 +7,7 @@ function About({ bio, links }) {
       <h2>About Me</h2>
       {bio ? (<p>{bio}</p>) : null}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links github={links.github} linkedin={links.linkedin} />
     </div>
   );
 }
